a832622a47b095c8adeb2e02083022c20ee8e5d2
priman <priman567@gmail.com>
2023-06-07 22:32:11 +0530

    min-salary coonect to front end

--- front-end/dist/index.18dbc454.js
+++ front-end/dist/index.18dbc454.js
@@ -574,37 +574,26 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
-// document.getElementById("salary")
-// let salary = document.getElementById("salary").innerText;
-// let salary = $("#salary12").val();
-// let apiUrl = `http://localhost:8000/tax/calculate?salary=${salary}`;
-// // console.log(salary)
-// taxBtnElm.on('click',()=>{
-//     console.log(salary)
-//     // calculateTax();
-// });
-//
-// function calculateTax() {
-//
-//     const jqxhr = $.ajax(apiUrl);
-//     jqxhr.done((salary1) => {
-//         console.log(salary1);
-//         taxShowElm.text(salary1);
-//     });
-//     jqxhr.fail(() => {
-//     });
-// }
 $(document).ready(function() {
     let taxShowElm = $("#result");
     let taxButton = $("#tax");
     let etfButton = $("#etf");
     let tableRow = $("#eft-row");
+    let setMinBtn = $("#min-salary-btn");
+    let minSalaryElm = $("#min-salary");
     // Attach an event listener to the button
     taxButton.on("click", function() {
         // Get the value of the input field
         let value = $("#salary12").val();
         let apiUrl = `http://localhost:8000/tax/calculate?salary=${value}`;
         calculateTax(apiUrl);
+    });
+    setMinBtn.on("click", function() {
+        // Get the value of the input field
+        console.log("wada");
+        let value = $("#minSalaryElm").val();
+        let apiUrl = `http://localhost:8000/application`;
+        setMinSalary(apiUrl);
     });
     etfButton.on("click", function() {
         // Get the value of the input field
@@ -620,6 +609,19 @@ $(document).ready(function() {
             taxShowElm.text(`Rs:${salary1}`);
         });
         jqxhr.fail(()=>{});
+    }
+    function setMinSalary(apiUrl) {
+        const requestBody = {
+            propertyname: "minSalary",
+            propertyvalue: minSalaryElm.val()
+        };
+        console.log(requestBody);
+        const jqxhr = $.ajax(apiUrl, {
+            method: "POST",
+            data: JSON.stringify(requestBody),
+            contentType: "application/json",
+            processData: false // by default jQuery tries to convert the data into String
+        });
     }
     function calculateETF(apiUrl) {
         const jqxhr = $.ajax(apiUrl);
--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -1,44 +1,30 @@
+$(document).ready(function () {
 
-
-
-// document.getElementById("salary")
-// let salary = document.getElementById("salary").innerText;
-// let salary = $("#salary12").val();
-// let apiUrl = `http://localhost:8000/tax/calculate?salary=${salary}`;
-// // console.log(salary)
-// taxBtnElm.on('click',()=>{
-//     console.log(salary)
-//     // calculateTax();
-// });
-//
-// function calculateTax() {
-//
-//     const jqxhr = $.ajax(apiUrl);
-//     jqxhr.done((salary1) => {
-//         console.log(salary1);
-//         taxShowElm.text(salary1);
-//     });
-//     jqxhr.fail(() => {
-//     });
-// }
-$(document).ready(function() {
-
-let taxShowElm = $("#result");
+    let taxShowElm = $("#result");
     let taxButton = $("#tax");
     let etfButton = $("#etf");
     let tableRow = $("#eft-row");
-
+    let setMinBtn = $("#min-salary-btn");
+    let minSalaryElm = $("#min-salary");
 
 
     // Attach an event listener to the button
-    taxButton.on("click", function() {
+    taxButton.on("click", function () {
         // Get the value of the input field
         let value = $("#salary12").val();
         let apiUrl = `http://localhost:8000/tax/calculate?salary=${value}`;
         calculateTax(apiUrl);
 
     });
-    etfButton.on("click", function() {
+    setMinBtn.on("click", function () {
+        // Get the value of the input field
+        console.log("wada")
+        let value = $("#minSalaryElm").val();
+        let apiUrl = `http://localhost:8000/application`;
+        setMinSalary(apiUrl);
+
+    });
+    etfButton.on("click", function () {
         // Get the value of the input field
         let value = $("#emplyee").val();
         console.log(value)
@@ -48,32 +34,47 @@ let taxShowElm = $("#result");
     });
 
 //
-function calculateTax(apiUrl) {
+    function calculateTax(apiUrl) {
 
-    const jqxhr = $.ajax(apiUrl);
-    jqxhr.done((salary1) => {
-        taxShowElm.text(`Rs:${salary1}`);
-    });
-    jqxhr.fail(() => {
-    });
-}
-function calculateETF(apiUrl) {
+        const jqxhr = $.ajax(apiUrl);
+        jqxhr.done((salary1) => {
+            taxShowElm.text(`Rs:${salary1}`);
+        });
+        jqxhr.fail(() => {
+        });
+    }
 
-    const jqxhr = $.ajax(apiUrl);
-    jqxhr.done((etfList) => {
-       tableRow.empty();
-        etfList.forEach(value=>{
+    function setMinSalary(apiUrl) {
+        const requestBody = {
+            propertyname: "minSalary",
+            propertyvalue: minSalaryElm.val()
+        };
+        console.log(requestBody);
+        const jqxhr = $.ajax(apiUrl, {
+            method: 'POST',
+            data: JSON.stringify(requestBody),
+            contentType: "application/json",       // by default jQuery uses application/x-www-form-urlencoded
+            processData: false          // by default jQuery tries to convert the data into String
+        });
+    }
 
-            console.log(value)
-            tableRow.append(`<td>${value.takeHomeSalary}</td>`);
-            tableRow.append(`<td>${value.etf}</td>`);
-            tableRow.append(`<td>${value.epf}</td>`);
+        function calculateETF(apiUrl) {
 
-        })
-    });
-    jqxhr.fail(() => {
-    });
-}
+            const jqxhr = $.ajax(apiUrl);
+            jqxhr.done((etfList) => {
+                tableRow.empty();
+                etfList.forEach(value => {
+
+                    console.log(value)
+                    tableRow.append(`<td>${value.takeHomeSalary}</td>`);
+                    tableRow.append(`<td>${value.etf}</td>`);
+                    tableRow.append(`<td>${value.epf}</td>`);
+
+                })
+            });
+            jqxhr.fail(() => {
+            });
+        }
 });
 
 
